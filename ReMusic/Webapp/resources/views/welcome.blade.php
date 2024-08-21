6bccffb2ffe46b69e354799e5e523101dd84918e
Leonel Gonzalez <leonel.gvalencia@alumnos.udg.mx>
2024-08-21 11:03:55 -0600

    Estetica mejorada

--- ReMusic/Webapp/resources/views/welcome.blade.php
+++ ReMusic/Webapp/resources/views/welcome.blade.php
@@ -1,48 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    </head>
-    <body>
+		<title>Laravel</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+		<style>
+			body {
+				background-image: url('/image/background.jpeg');
+				background-position: center center;
+				background-repeat: no-repeat;
+				background-size: 150em;
+			}
+		</style>
+	</head>
+	<body>
+		<div>
+			<div class="p-2 m-2 bg-info text-white shadow rounded-2">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">ReMusic</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Opciones</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Historial</a>
+						</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
 
-        <div>        
-            <div class="p-2 m-2 bg-info text-white shadow rounded-2">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
+		<div class="d-flex align-items-center justify-content-center flex-column">
+			<div class="p-2 m-2 bg-info text-white shadow rounded-2">
+				<label for="formFileLg" class="form-label">Sube tu Imagen</label>
+				<input class="form-control form-control-lg" id="formFileLg" type="file">
+			</div>
+			<div class="p-2 m-2 bg-info text-white shadow rounded-2">
+				<button type="button" class="btn btn-primary">Upload</button>
+			</div>
+		</div>
 
-        <div class="d-flex align-items-center justify-content-center flex-column">
-            <div class="p-2 m-2 bg-info text-white shadow rounded-2">
-                <label for="formFileLg" class="form-label">Sube tu Imagen</label>
-                <input class="form-control form-control-lg" id="formFileLg" type="file">
-            </div>
-            <div class="p-2 m-2 bg-info text-white shadow rounded-2">
-                <button type="button" class="btn btn-primary">Upload</button>
-            </div>
-        </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	</body>
 </html>
