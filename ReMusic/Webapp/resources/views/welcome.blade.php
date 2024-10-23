6d3589d539a21fd42d3b35248a3afae00a323a2e
Leonel Gonzalez <leonel.gvalencia@alumnos.udg.mx>
2024-10-23 15:59:04 -0600

    Cambios al CSS para mejorar tiempo de carga

--- ReMusic/Webapp/resources/views/welcome.blade.php
+++ ReMusic/Webapp/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 		<style>
 			body {
-				background-image: url('/image/background.jpeg');
+				background-color: lightgray;
 				background-position: center center;
 				background-repeat: no-repeat;
 				background-size: 150em;
